0dde3cc71a7d505a8646e5aa3136a29749ddcfad
priyankasairise <priyankasairise@gmail.com>
2022-11-06 16:24:14 +0530

    Initial Version

---
+++ src/array_prpgrams/ArrayTest2.java
@@ -0,0 +1,20 @@
+package array_prpgrams;
+import java.util.Scanner;
+public class ArrayTest2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        float[] marks=new float[5];
+        System.out.println("please enter marks for 5 students");
+        for(int i=0;i< marks.length;i++)
+        {
+            marks[i]=sc.nextFloat();
+        }
+        System.out.println("marks are as follow: ");
+        for(int i=0;i<marks.length;i++)
+        {
+            System.out.println(marks[i]);
+        }
+    }
+}
+
